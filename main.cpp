e70fe91c84099c1c3c57a743f0e70527c1d149cb
Bhavya Singh <singhbhavya037@gmail.com>
2024-08-27 16:46:27 +0530

    added name & health

--- main.cpp
+++ main.cpp
@@ -3,7 +3,27 @@
 
 using namespace std;
 
-int main(){
+class Character {
+private:
+    string name;
+    int health;
 
+public:
+    Character(string characterName) : name(characterName), health(100) {}
 
+    void displayStatus() {
+        cout << "Character: " << name << endl;
+        cout << "Health: " << health << endl;
+    }
+};
+
+int main() {
+    string playerName;
+    cout << "Enter your character's name: ";
+    getline(cin, playerName);
+
+    Character player(playerName);
+    player.displayStatus();
+
+    return 0;
 }
